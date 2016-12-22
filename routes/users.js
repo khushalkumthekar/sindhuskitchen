c6d61cb2aab0ca2809d51b6c4816b16125097828
Khushal Kumthekar <khushal@Khushals-MacBook-Air.local>
2016-12-22 01:25:35 -0800

    modified

--- routes/users.js
+++ routes/users.js
@@ -1,23 +1,16 @@
 var express = require('express');
 var router = express.Router();
 var https = require('https');
-var HttpsProxyAgent = require('https-proxy-agent');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  var responseString = "";
-  var agent = new HttpsProxyAgent({
-    host: 'webproxysea.nordstrom.net',
-    port: 8181
-  });
+
   
   var optionsget = {
     host : 'graph.facebook.com', // here only the domain name
     // (no http/https !)
     port : 443,
     path : '/v2.8/sindhuskitchen/posts?limit=50&fields=message%2Ccreated_time%2Cid%2Cfull_picture&access_token=EAACEdEose0cBAEiAE1LqbQcWj7Lsen4GtUe0lpKtS6NmzcP0FZB2HhAB95Wrog7Y1FSKqq9VFkGhSk3OaECZCll5uq2rjsJ5iGGj7lMhLrXj5QnmikoJtrKyCRB7Kt2J1OCXO5xixxH3E1PZCAz6g0uY0xf0kcjX54VfDnOSZC4IxAexRcb2O9t2poOYpkYZD', // the rest of the url with parameters if needed
-    method : 'GET', // do GET
-	agent: agent
+    method : 'GET' // do GET
   } ;
   
   console.info('Options prepared:');
