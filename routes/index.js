183e3083e56520332ca1a1bd1f9642245bd19e8f
Khushal Kumthekar <khushal@Khushals-MacBook-Air.local>
2016-12-22 01:31:13 -0800

    modified

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,24 @@
 var express = require('express');
 var router = express.Router();
 var https = require('https');
-var HttpsProxyAgent = require('https-proxy-agent');
+//var HttpsProxyAgent = require('https-proxy-agent');
+var dateFormat = require('dateformat');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var responseString = "";
-  var agent = new HttpsProxyAgent({
-    host: 'webproxysea.nordstrom.net',
-    port: 8181
-  });
+  //var agent = new HttpsProxyAgent({
+    //host: 'webproxysea.nordstrom.net',
+    //port: 8181
+  //});
   
   var optionsget = {
     host : 'graph.facebook.com', // here only the domain name
     // (no http/https !)
     port : 443,
-    path : '/v2.8/sindhuskitchen/posts?limit=50&fields=message%2Ccreated_time%2Cid%2Cfull_picture&access_token=EAACEdEose0cBAEiAE1LqbQcWj7Lsen4GtUe0lpKtS6NmzcP0FZB2HhAB95Wrog7Y1FSKqq9VFkGhSk3OaECZCll5uq2rjsJ5iGGj7lMhLrXj5QnmikoJtrKyCRB7Kt2J1OCXO5xixxH3E1PZCAz6g0uY0xf0kcjX54VfDnOSZC4IxAexRcb2O9t2poOYpkYZD', // the rest of the url with parameters if needed
+    path : '/v2.8/sindhuskitchen/posts?limit=50&fields=message%2Ccreated_time%2Cid%2Cfull_picture&access_token=EAACEdEose0cBAHehVIsMuVm1fKwJKYQeVosoijnZBoynGKPZAU1SnyZByYhsVngNjo2ud9IZAbi9RZADdgKAP6I87ZALHrdmHAKKjP3UqN0jbGVnys8duYlNoQVxKSZAO7sJdq3OahGnaDCGKG4Rfd6r8tQE15YMNhs5n8tqKJZCSLbV73B3v33Y', // the rest of the url with parameters if needed
     method : 'GET', // do GET
-	agent: agent
+	//agent: agent
   } ;
   
   console.info('Options prepared:');
@@ -47,9 +48,11 @@ router.get('/', function(req, res, next) {
 	  responseJson.data.forEach (function(post) {
 		  var msg =  post.message;
 		  if(msg){
-		    process.stdout.write(msg);
 			if(msg.indexOf('christmas')>-1){
-			posts.push(post);
+               var postDate = Date.parse(post.created_time);
+               post.message = msg.replace('December is here! Countdown to Christmas starts now. ', '').substring(15);
+               post.created_time = dateFormat(postDate,"longDate");
+			   posts.push(post);
 			}
 		  }
 	  });
